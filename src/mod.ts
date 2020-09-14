a7d32bdc8299787ed0f442b033a0d630eaedbcda
Edwin Torres <codesandtags@gmail.com>
2020-09-14 18:11:53 -0500

    feature: Added routes and data

--- src/mod.ts
+++ src/mod.ts
@@ -1,26 +1,31 @@
-import { Application } from "https://deno.land/x/oak@v6.1.0/mod.ts";
+import { log, Application, send } from "./deps.ts";
+
+import api from "./api.ts";
 
 const app = new Application();
 const PORT = 8000;
 
-app.use(async (ctx, next) => {
-  await next();
-  const time = ctx.response.headers.get("X-Response-Time");
-  console.log(`${ctx.request.method} ${ctx.request.url} ${time}`);
-  console.log("With Async");
-});
+app.use(api.routes());
+app.use(api.allowedMethods());
 
-app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const delta = Date.now() - start;
-  ctx.response.headers.set("X-Response-Time", `${delta}ms`);
-  console.log(` Response time ${delta}ms`);
-});
-
+/**
+ * Send public files
+ */
 app.use(async (ctx) => {
-  ctx.response.body = "This is sparta :D!!!";
-  console.log("With message");
+  const PUBLIC = "/public";
+  const filePath = ctx.request.url.pathname;
+  const fileWhitelist = [
+    "/index.html",
+    "/javascripts/script.js",
+    "/stylesheets/style.css",
+    "/images/favicon.png",
+  ];
+
+  if (fileWhitelist.includes(filePath)) {
+    await send(ctx, filePath, {
+      root: `${Deno.cwd()}${PUBLIC}`,
+    });
+  }
 });
 
 app.listen({
